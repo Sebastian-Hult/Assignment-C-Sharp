23f25d151e79dc45322ddd0ca790ae366d63c226
Sebastian-Hult <sebastian.hult@utb.ecutbildning.se>
2024-12-19 15:51:04 +0100

    Forth commit: Created IFileService, FileService and ContactSerializer, with functionality, in the respective folders.

---
+++ Business/Helpers/ContactSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using Business.Interfaces;
+using Business.Models;
+
+namespace Business.Helpers;
+
+public class ContactSerializer(IFileService fileService)
+{
+    private readonly IFileService _fileService = fileService;
+
+    public bool SaveContacts(List<Contact> contacts)
+    {
+        var json = JsonSerializer.Serialize(contacts);
+        return _fileService.SaveContentToFile(json);
+    }
+
+    public List<Contact> GetContacts()
+    {
+        var json = _fileService.GetContentFromFile();
+        var contacts = JsonSerializer.Deserialize<List<Contact>>(json);
+        return contacts;
+    }
+}
---
+++ Business/Services/FileService.cs
@@ -0,0 +1,32 @@
+﻿using Business.Interfaces;
+
+namespace Business.Services;
+
+public class FileService : IFileService
+{
+    private readonly string _directoryPath;
+    private readonly string _filePath;
+
+    public FileService(string directoryPath, string fileName)
+    {
+        _directoryPath = directoryPath;
+        _filePath = Path.Combine(_directoryPath, fileName);
+    }
+
+    public string GetContentFromFile()
+    {
+        if (File.Exists(_filePath))
+            return File.ReadAllText(_filePath);
+
+        return null!;
+    }
+
+    public bool SaveContentToFile(string content)
+    {
+        if (!Directory.Exists(_directoryPath))
+            Directory.CreateDirectory(_directoryPath);
+
+        File.WriteAllText(_filePath, content);
+        return true;
+    }
+}
