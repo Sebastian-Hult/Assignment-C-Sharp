eae0031b0f7fe64aac913eeddc3fb2656f1ae9b4
Sebastian-Hult <sebastian.hult@utb.ecutbildning.se>
2024-12-19 16:33:06 +0100

    Eighth commit: Created an instance for the MainMenu() function in class MenuDialog.

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine(".");
+﻿using Business.Helpers;
+using Business.Services;
+using MainApp.Dialogs;
+
+var fileService = new FileService("Data", "contact.json");
+var contactSerializer = new ContactSerializer(fileService);
+var contactService = new ContactService(contactSerializer);
+var menuDialog = new MenuDialog(contactService);
+menuDialog.MainMenu();
