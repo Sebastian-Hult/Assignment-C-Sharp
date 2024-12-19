e2c10fb829d66b2c1239bc46007f7c2c0e90d12f
Sebastian-Hult <sebastian.hult@utb.ecutbildning.se>
2024-12-19 15:31:28 +0100

    Third commit: Created an interface IContactService and a class ContactService in their respective folders.

---
+++ Business/Services/ContactService.cs
@@ -0,0 +1,17 @@
+﻿using Business.Interfaces;
+using Business.Models;
+
+namespace Business.Services;
+
+public class ContactService : IContactService
+{
+    public bool CreateContact(Contact contact)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Contact> GetAllContacts()
+    {
+        throw new NotImplementedException();
+    }
+}
