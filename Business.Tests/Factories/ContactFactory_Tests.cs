2221f70c1593e8620cb1c9fce81bc21cac1b0a98
Sebastian-Hult <sebastian.hult@utb.ecutbildning.se>
2024-12-20 14:42:46 +0100

    Eleventh commit: Added functionality to ContactFactory_Tests.

--- Business.Tests/Factories/ContactFactory_Tests.cs
+++ Business.Tests/Factories/ContactFactory_Tests.cs
@@ -1,5 +1,20 @@
-﻿namespace Business.Tests.Factories;
+﻿using Business.Factories;
+using Business.Models;
+
+namespace Business.Tests.Factories;
 
 public class ContactFactory_Tests
 {
+    [Fact]
+    public void CreateNewContact_ShouldReturnContact()
+    {
+        // Arrange
+
+        // Act
+        var result = ContactFactory.CreateNewContact();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<Contact>(result);
+    }
 }
